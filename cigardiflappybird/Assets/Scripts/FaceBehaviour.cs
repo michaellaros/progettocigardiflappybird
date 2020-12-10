22a7b444acaa21f7d0a7681bceb02e50d1ca9ef9
DavideWu <56259661+DavideWu@users.noreply.github.com>
2020-12-10 18:20:13 +0100

    ARupdate

---
+++ cigardiflappybird/Assets/Scripts/FaceBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class FaceBehaviour : MonoBehaviour
+{
+    MeshFilter myMeshFilter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMeshFilter = GetComponent<MeshFilter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myMeshFilter.mesh != null)
+        {
+            print(myMeshFilter.mesh.vertexCount);
+        }
+    }
+}
